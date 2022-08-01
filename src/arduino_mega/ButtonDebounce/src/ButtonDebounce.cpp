b14824098ee10cf90f3c06587a0d97f56a71377c
christophmoelzer <christoph.moelzer@technikum-wien.at>
2022-08-01 08:34:48 +0200

    libraries added

---
+++ src/arduino_mega/ButtonDebounce/src/ButtonDebounce.cpp
@@ -0,0 +1,43 @@
+#include "Arduino.h"
+#include "ButtonDebounce.h"
+
+ButtonDebounce::ButtonDebounce(){
+  ;
+}
+
+ButtonDebounce::ButtonDebounce(int pin, unsigned long delay){
+  pinMode(pin, INPUT_PULLUP);
+  _pin = pin;
+  _delay = delay;
+  _lastDebounceTime = 0;
+  _lastStateBtn = HIGH;
+}
+
+void ButtonDebounce::init(int pin, unsigned long delay){
+  pinMode(pin, INPUT_PULLUP);
+  _pin = pin;
+  _delay = delay;
+  _lastDebounceTime = 0;
+  _lastStateBtn = HIGH;
+}
+
+bool ButtonDebounce::isTimeToUpdate(){
+  return (millis() - _lastDebounceTime) > _delay;
+}
+
+void ButtonDebounce::update(){
+  if(!isTimeToUpdate()) return;
+  _lastDebounceTime = millis();
+  int btnState = digitalRead(_pin);
+  if(btnState == _lastStateBtn) return;
+  _lastStateBtn = btnState;
+  if(this->callback) this->callback(_lastStateBtn);
+}
+
+int ButtonDebounce::state(){
+  return _lastStateBtn;
+}
+
+void ButtonDebounce::setCallback(BTN_CALLBACK){
+  this->callback = callback;
+}
